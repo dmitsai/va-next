4d937205780da47e1f1626d4671c59c1d10ff69a
Parkhomenko Egor <54312766+1MPULSEONE@users.noreply.github.com>
2024-09-18 21:49:48 +0600

    [Feat] WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,23 @@
 import { useEffect } from "react";
 import Toaster from "~/entities/sonner/ui/index";
 import { toast } from "sonner";
+import IconSonnerInfo from "~/shared/assets/icons/icon-sonner-info.svg";
 
 export default () => {
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     toast("Error", {
       duration: 10000,
-      id: "test-id",
+      description: "Your session has expired. Please log in again.  ",
+      icon: <IconSonnerInfo />,
+      closeButton: true,
     });
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center text-black">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-black">
       <div className="flex flex-col items-center justify-center">
-        <Toaster type="error" />
+        <Toaster type="info" />
       </div>
     </main>
   );
--- src/entities/sonner/ui/Sonner.tsx
+++ src/entities/sonner/ui/Sonner.tsx
@@ -10,22 +10,23 @@ interface IToaster {
   type: "error" | "info";
 }
 
-const Toaster = ({ ...props }: ToasterProps & IToaster) => {
+const Toaster = ({ type, ...props }: ToasterProps & IToaster) => {
   const { theme = "system" } = useTheme();
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme}
       className="toaster group"
       toastOptions={{
         classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-base-dark group-[.toaster]:text-foreground group-[.toaster]:shadow-lg small",
-          description: "group-[.toast]:text-muted-foreground",
+          toast: `group toast group-[.toaster]:bg-base-dark group-[.toaster]:text-text-dark group-[.toaster]:p-[0.75rem] rounded-6 small ${type === "error" ? "border-red-dark" : "border-mauve-dark"}`,
+          description:
+            "group-[.toast]:text-sub-secondary-dark/70 group-[.toaster]:text-12",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          icon: "group-[.toast]:self-start group-[.toast]:translate-y-1 group-[.toast]:translate-x-1",
+          closeButton:
+            "group-[.toaster]:bg-base-dark group-[.toast]:text-muted-foreground group-[.toast]:border-none group-[.toast]:left-0 group-[.toast]:right-[10px]",
         },
       }}
       {...props}
