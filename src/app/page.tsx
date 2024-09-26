2153f5eb05579ba7dcdd7556caab0f9ab754febe
dmitsai <dmitriisai456@gmail.com>
2024-09-26 16:00:20 +0600

    [feat] add hover for btns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,20 @@
 import Link from "next/link";
 import {Button, ButtonView} from "~/shared/ui/Button";
 import SettingsIcon from  '~/shared/assets/icons/settings-icon.svg';
+import SettingLightIcon from  '~/shared/assets/icons/settings-light-icon.svg';
  import DeleteIcon from  '~/shared/assets/icons/delete-icon.svg';
 
 export default () => (
     <main
         className="flex min-h-screen flex-col gap-y-4   items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-
-     <Button buttonView={ButtonView.SMALL} className={'text-base-dark bg-mauve-dark'}>
+     <Button buttonView={ButtonView.SMALL} className={'text-base-dark bg-mauve-dark hover:bg-text-dark transition-colors'}>
        {'Sign up / Log in'}
      </Button>
-      <Button buttonView={ButtonView.LARGE} className={'!p-3 bg-mantle-dark'}  >
-          <SettingsIcon  className={'w-full h-full'}/>
+      <Button buttonView={ButtonView.LARGE} className={'group !p-3 bg-mantle-dark hover:bg-text-dark w-10 transition-colors'}  >
+          <SettingsIcon  className={'absolute w-4 h-4 visible group-hover:invisible'}/>
+          <SettingLightIcon  className={'absolute w-4 h-4 invisible group-hover:visible'}/>
       </Button>
-        <Button buttonView={ButtonView.LARGE} className={'bg-red-dark'}>
+        <Button buttonView={ButtonView.LARGE} className={'bg-red-dark hover:bg-text-dark transition-colors'}>
             <DeleteIcon className={'w-full h-full'}/>
             <span className={'text-base-dark'}>{'Delete'}</span>
         </Button>
