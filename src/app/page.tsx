dfd77a1f903fe642ce0d2d7150b43c7a164104b4
dmitsai <dmitriisai456@gmail.com>
2024-09-26 15:45:37 +0600

    [feat] add WIP view of button on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,20 @@
 import Link from "next/link";
 import {Button, ButtonView} from "~/shared/ui/Button";
 import SettingsIcon from  '~/shared/assets/icons/settings-icon.svg';
-// import DeleteIcon from  '~/shared/assets/icons/delete-icon.svg';
+ import DeleteIcon from  '~/shared/assets/icons/delete-icon.svg';
 
 export default () => (
     <main
         className="flex min-h-screen flex-col gap-y-4   items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <SettingsIcon/>
+
      <Button buttonView={ButtonView.SMALL} className={'text-base-dark bg-mauve-dark'}>
        {'Sign up / Log in'}
      </Button>
-      <Button buttonView={ButtonView.LARGE} >
-          {'dsffs'}
+      <Button buttonView={ButtonView.LARGE} className={'!p-3 bg-mantle-dark'}  >
+          <SettingsIcon  className={'w-full h-full'}/>
       </Button>
         <Button buttonView={ButtonView.LARGE} className={'bg-red-dark'}>
-            {/*<DeleteIcon/>*/}
-            <div className={'w-4 h-4 rounded-full flex items-center justify-center bg-mantle-dark'}>{'!'}</div>
+            <DeleteIcon className={'w-full h-full'}/>
             <span className={'text-base-dark'}>{'Delete'}</span>
         </Button>
     </main>
