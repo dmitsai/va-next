ab1f017539c91ec8c22e83fd9c73a7b91123038d
Parkhomenko Egor <parkhomenko2049@gmail.com>
2024-10-03 01:49:27 +0600

    [Fix] Fix SVGR usage in storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default () => (
-  <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-    <div className="flex flex-col items-center justify-center" />
-  </main>
-);
+"use client";
+
+import { useEffect } from "react";
+import IconSonnerError from "~/shared/assets/icons/icon-sonner-error.svg?url";
+
+export default () => {
+  useEffect(() => console.log(IconSonnerError), []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <div className="flex flex-col items-center justify-center" />
+    </main>
+  );
+};
