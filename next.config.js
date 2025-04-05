7de038c0403ad3e8d31e40332b8601e3faba27e5
dmitsai <dmitriisai456@gmail.com>
2025-04-05 17:10:52 +0600

    [fix] fix next config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+
+  optimizeFonts: false,
+  
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
