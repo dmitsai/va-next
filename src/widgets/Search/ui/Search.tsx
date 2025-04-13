37e68df8f24083485a6cdd36acfdc69abb17a801
dmitsai <dmitriisai456@gmail.com>
2025-04-13 21:12:30 +0600

    [fix] remove temp data

--- src/widgets/Search/ui/Search.tsx
+++ src/widgets/Search/ui/Search.tsx
@@ -11,30 +11,7 @@ import Select from "~/shared/ui/Select";
 import { ReactComponent as IconSettings } from "~/shared/assets/icons/settings-icon.svg";
 import { ReactComponent as SearchIcon } from '~/shared/assets/icons/search-icon.svg';
 import { FilterMenu } from '~/features/filterMenu';
-
-
-const tempFilters = [
-    { name: 'speciality', localTitle: 'Специальность', value: [{ name: 'webDeveloper', localTitle: 'Web-разработчик' }, { name: 'backend', localTitle: 'Backend-разработчик' }] },
-    { name: 'workSchedule', localTitle: 'График работы', value: [{ name: 'fullDay', localTitle: 'Полный день' }, { name: 'flexible', localTitle: 'Гибкий график' }] },
-    { name: 'experience', localTitle: 'Опыт', value: [{ name: 'lowerOneYear', localTitle: 'менее 1 года' }, { name: '1-3Years', localTitle: '1-3 года' }] },
-    { name: 'education', localTitle: 'Образование', value: [{ name: 'higher', localTitle: 'Высшие образование' }, { name: 'noEducation', localTitle: 'Нет образования' }] },
-    { name: 'employmentType', localTitle: 'тип занятости', value: [{ name: 'full', localTitle: 'Полная занятость' }, { name: 'parttime', localTitle: 'Частичная занятость' }] },
-];
-
-const regions = [
-    { name: 'moscow', localTitle: 'Москва' },
-    { name: 'omsk', localTitle: 'Омск' },
-    { name: 'samara', localTitle: 'Самара' },
-    { name: 'all', localTitle: 'Все регионы' },
-];
-
-const periods = [
-    { name: 'day', localTitle: 'День' },
-    { name: 'mouth', localTitle: 'Месяц' },
-    { name: '3mouth', localTitle: '3 месяца' },
-    { name: 'all', localTitle: 'Все время' },
-]
-const state = ['Вакансии', 'Стажировки'];
+import { periods, regions, state, tempFilters } from '../model/data';
 
 export interface SearchForm {
     search: string,
