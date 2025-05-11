580ea1f6cf1b4a74c665c8b3d999d06838c3835d
dmitsai <dmitriisai456@gmail.com>
2025-05-11 23:19:40 +0600

    [fix] resolve build errors

--- trpc/client.tsx
+++ trpc/client.tsx
@@ -1,67 +1,78 @@
 'use client';
 
-import { QueryClient, QueryClientProvider, HydrationBoundary  } from '@tanstack/react-query';
-import { httpBatchLink,httpLink,isNonJsonSerializable,loggerLink, splitLink } from '@trpc/client';
+import {
+    QueryClient,
+    QueryClientProvider,
+    HydrationBoundary,
+} from '@tanstack/react-query';
+import {
+    httpBatchLink,
+    httpLink,
+    isNonJsonSerializable,
+    loggerLink,
+    splitLink,
+} from '@trpc/client';
 import React, { ReactNode, useState } from 'react';
 import { AppRouter } from '~/server/api/root';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
-import {createTRPCReact} from '@trpc/react-query';
+import { createTRPCReact } from '@trpc/react-query';
 import { getUrl, makeQueryClient, transformer } from './shared';
 
-
-export const { TRPCProvider, useTRPC, useTRPCClient } = createTRPCContext<AppRouter>();
+export const { TRPCProvider, useTRPC, useTRPCClient } =
+    createTRPCContext<AppRouter>();
 
 let browserQueryClient: QueryClient;
 function getQueryClient() {
+    if (typeof window === 'undefined') {
+        return makeQueryClient();
+    }
 
-  if (typeof window === 'undefined') {
-    return makeQueryClient();
-  }
-  
-  if (!browserQueryClient) browserQueryClient = makeQueryClient();
-  return browserQueryClient;
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
 }
 
 export const clientApi = createTRPCReact<AppRouter>();
 
-interface ClientApiProviderProps  {
-  children: ReactNode;
-  dehydratedState?: unknown; 
-};
-
-export const ClientApiProvider: React.FC<ClientApiProviderProps> = ({children,dehydratedState}) => {
-  
-  const queryClient =getQueryClient();
-  const [trpcClient] = useState(() =>
-    clientApi.createClient({
-      links: [
-        loggerLink({
-          enabled: (op) => 
-            process.env.NODE_ENV === 'development' ||
-            op.direction === 'out' ||
-            op.type === 'mutation',
-        }),
-        splitLink({
-          condition: (op) => isNonJsonSerializable(op.input),
-          true: httpLink({
-            url: getUrl(),
-            transformer,
-          }),
-          false: httpBatchLink({
-            url: getUrl(),
-            transformer,
-          }),
-        }),
-      ],
-    }),
-  );
-  return (
-    <clientApi.Provider client={trpcClient} queryClient={queryClient} >
-      <QueryClientProvider client={queryClient}>
-          <HydrationBoundary state={dehydratedState}>
-            {children}
-          </HydrationBoundary>
-      </QueryClientProvider>
-    </clientApi.Provider>
-  );
+interface ClientApiProviderProps {
+    children: ReactNode;
+    dehydratedState?: unknown;
 }
+
+export const ClientApiProvider: React.FC<ClientApiProviderProps> = ({
+    children,
+    dehydratedState,
+}) => {
+    const queryClient = getQueryClient();
+    const [trpcClient] = useState(() =>
+        clientApi.createClient({
+            links: [
+                loggerLink({
+                    enabled: (op) =>
+                        process.env.NODE_ENV === 'development' ||
+                        op.direction === 'down' ||
+                        op.type === 'mutation',
+                }),
+                splitLink({
+                    condition: (op) => isNonJsonSerializable(op.input),
+                    true: httpLink({
+                        url: getUrl(),
+                        transformer,
+                    }),
+                    false: httpBatchLink({
+                        url: getUrl(),
+                        transformer,
+                    }),
+                }),
+            ],
+        })
+    );
+    return (
+        <clientApi.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                <HydrationBoundary state={dehydratedState}>
+                    {children}
+                </HydrationBoundary>
+            </QueryClientProvider>
+        </clientApi.Provider>
+    );
+};
